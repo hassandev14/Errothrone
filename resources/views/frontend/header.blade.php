d3a0192b6fe557a57f32d44ef6d9c42dc67997f8
HassaanNasir <hassantayyabdev@gmail.com>
2025-02-17 02:11:53 +0500

    issues resolved

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -165,7 +165,12 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
-                
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
+                </li>
                 <!-- Loop through categories for dropdown -->
                 @foreach($attributeCategories as $attributeCategory)
                 <li class="nav-item dropdown">
